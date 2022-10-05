efb77fffcb5fc7fbf301bf006f34eb98dce84dc5
Lucas H <lhatfield1@my.wctc.edu>
2022-10-05 18:50:10 -0500

    Add project files.

---
+++ RaceTrack/Cars/Moped.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    public class Moped : RaceCar
+    {
+        public Moped()
+        {
+            Name = "Moped";
+            TopSpeed = 45;
+        }
+
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The {Name} quietly starts");
+        }
+
+        public override void Brake()
+        {
+            Console.WriteLine($"The {Name} breaks too fast causing Morton to fall over the front!");
+            base.Brake();
+        }
+
+    }
+}
